d28138c1e88ecdc01563f2bf40d915e47363903a
Stephan Fuhrmann <s_fuhrm@gmx.de>
2017-09-28 22:59:30 +0200

    Fix for copyright

--- src/main/java/de/sfuhrm/radiobrowser/UrlResponse.java
+++ src/main/java/de/sfuhrm/radiobrowser/UrlResponse.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 fury.
+ * Copyright 2017 Stephan Fuhrmann.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
