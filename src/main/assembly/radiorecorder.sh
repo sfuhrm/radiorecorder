940cd3409fdc8b57e120b5b1d2ad8d6f543422e2
Stephan Fuhrmann <s@sfuhrm.de>
2020-02-23 15:12:28 +0100

    Output log dir in dist dir

--- src/main/assembly/radiorecorder.sh
+++ src/main/assembly/radiorecorder.sh
@@ -2,5 +2,5 @@
 
 BASEDIR=$(cd $(dirname "$0")/..; pwd)
 JAR=${BASEDIR}/lib/radiorecorder-*.jar
-
-java -Dlog4j.configurationFile=file:///${BASEDIR}/etc/log4j2.xml -jar ${JAR} "$@"
+LOG_DIR="${BASEDIR}/log"
+java -DLOG_DIR=${LOG_DIR} -Dlog4j.configurationFile=file:///${BASEDIR}/etc/log4j2.xml -jar ${JAR} "$@"
