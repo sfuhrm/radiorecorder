1b01052049dd924a7ba111f3def82b1616d6c69b
Stephan Fuhrmann <s@sfuhrm.de>
2021-01-03 15:38:28 +0100

    Custom docker images

--- helper/centos/package.sh
+++ helper/centos/package.sh
@@ -1,12 +1,6 @@
 MAVEN_VERSION=$(cat circle_maven_version.txt)
 RPM_VERSION=$(echo ${MAVEN_VERSION} | cut -d"-" -f1)
 
-curl --location https://github.com/AdoptOpenJDK/openjdk14-binaries/releases/download/jdk-14.0.2%2B12/OpenJDK14U-jdk_x64_linux_hotspot_14.0.2_12.tar.gz --output - |
-tar -xzvf-
-PATH=$PWD/jdk-14.0.2+12/bin:$PATH
-
-yum install --assumeyes rpm-build
-
 jpackage \
 --name radiorecorder \
 --type rpm \
--- helper/debian/package.sh
+++ helper/debian/package.sh
@@ -1,11 +1,5 @@
 VERSION=$(cat circle_maven_version.txt)
 
-apt-get update && apt-get install --assume-yes fakeroot curl binutils
-
-curl --location https://github.com/AdoptOpenJDK/openjdk14-binaries/releases/download/jdk-14.0.2%2B12/OpenJDK14U-jdk_x64_linux_hotspot_14.0.2_12.tar.gz --output - |
-tar -xzvf-
-PATH=$PWD/jdk-14.0.2+12/bin:$PATH
-
 jpackage \
 --name radiorecorder \
 --type deb \
