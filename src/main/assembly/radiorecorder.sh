b15101b1b7bde47bbe2295dbeff48ca886e26817
Stephan Fuhrmann <s@sfuhrm.de>
2021-12-12 16:24:39 +0100

    Use resource substitution

--- src/main/assembly/radiorecorder.sh
+++ src/main/assembly/radiorecorder.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 BASEDIR=$(cd $(dirname "$0")/..; pwd)
-JAR=${BASEDIR}/lib/radiorecorder-*.jar
+JAR=${BASEDIR}/lib/radiorecorder-${project.version}.jar
 LOG_DIR="${BASEDIR}/log"
 
 java -DLOG_DIR=${LOG_DIR} -Dlog4j.configurationFile=file:///${BASEDIR}/etc/log4j2.xml -jar ${JAR} "$@"
