a29b9ac6084c3c08d889d6ff11502a61c55bbae3
Stephan Fuhrmann <s@sfuhrm.de>
2022-02-06 15:55:10 +0100

    Logging props no more necessary

--- src/main/assembly/radiorecorder.sh
+++ src/main/assembly/radiorecorder.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
 BASEDIR=$(cd $(dirname "$0")/..; pwd)
-JAR=${BASEDIR}/lib/radiorecorder-${project.version}.jar
-LOG_DIR="${BASEDIR}/log"
 
-java -DLOG_DIR=${LOG_DIR} -Dlog4j.configurationFile=file:///${BASEDIR}/etc/log4j2.xml -jar ${JAR} "$@"
+java -jar ${JAR} "$@"
